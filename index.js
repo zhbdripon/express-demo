b4da38205e96c579628ece5f9aab57a16b1a14db
zhbdripon <zhbdripon@gmail.com>
2020-05-11 16:48:41 +0600

    request validation using Joi

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses',(req,res)=>{
-    if(!req.body.name){
-        res.status(400).send('Bad request, name required');
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body,schema);
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
         return;
     }
     const course = {
