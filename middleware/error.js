e943c268bd43f65dc66dc1a41dc466fc19325654
zhbdripon <zhbdripon@gmail.com>
2020-05-20 06:52:33 +0600

    handled json parse error

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,8 @@
 module.exports = function (err, req, res, next) {
-    if(err.name=='CastError' && err.path =='_id'){
-        res.status(400).send('id must be a string of 24 hex characters')
+    if (err.type =='entity.parse.failed'){
+        res.status(400).send('Entity parse failed, Invalid Json');
+    }if(err.name=='CastError' && err.path =='_id'){
+        res.status(400).send('id must be a string of 24 hex characters');
     }else{
         res.status(500).send('Something Failed.');
     }
