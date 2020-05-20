977fb574d368e74236ccf621ecb07c065b4163a4
zhbdripon <zhbdripon@gmail.com>
2020-05-20 06:54:50 +0600

    added missing else

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 module.exports = function (err, req, res, next) {
     if (err.type =='entity.parse.failed'){
         res.status(400).send('Entity parse failed, Invalid Json');
-    }if(err.name=='CastError' && err.path =='_id'){
+    }else if(err.name=='CastError' && err.path =='_id'){
         res.status(400).send('id must be a string of 24 hex characters');
     }else{
         res.status(500).send('Something Failed.');
